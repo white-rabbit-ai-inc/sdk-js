9d258f98682066119eace30999b714b058b332bd
Tim Gosnell <timothgosnell@gmail.com>
2022-03-07 15:13:47 -0700

    updating connection

--- src/connection/connection.spec.js
+++ src/connection/connection.spec.js
@@ -9,9 +9,19 @@ describe('test connection', () => {
     expect(connection.environment).toBe(connection.requests[connection.ENVIRONMENT_TYPES.STAGING])
   })
 
-  test('get connection', () => {
+  test('get connection with env param', () => {
     expect(connection.init({ apiKey: '1234', environment: connection.ENVIRONMENT_TYPES.STAGING })).not.toBe(undefined)
     expect(connection.init()).toHaveProperty('environment')
+    expect(connection.environment).toBe(connection.requests[connection.ENVIRONMENT_TYPES.STAGING])
+  })
+
+  test('get connection', () => {
+    const conn = connection.init({ apiKey: '1234'})
+    expect(conn).not.toBe(undefined)
+    expect(conn).toHaveProperty('environment')
+    console.log(conn.environment)
+    console.log(connection.requests[connection.ENVIRONMENT_TYPES.PRODUCTION])
+    expect(conn.environment).toBe(connection.requests[connection.ENVIRONMENT_TYPES.PRODUCTION])
   })
 })
 
